d7902161be3ac0add0098ba45433f03d324f7468
davidbagh22 <davidbagh22@gmail.com>
2026-07-02 00:55:27 +0400

    Fix leader imports

--- app/handlers/leader/__init__.py
+++ app/handlers/leader/__init__.py
@@ -1,13 +1,9 @@
 from aiogram import Router
-
-from app.handlers.leader import event_builder, act, addons, open_tasks, panel, task_deadline_buttons
-
+from app.handlers.leader import event_builder, addons, open_tasks, panel, task_deadline_buttons
 router = Router(name="leader_root")
 router.include_router(event_builder.router)
-router.include_router(act.router)
 router.include_router(open_tasks.router)
 router.include_router(task_deadline_buttons.router)
 router.include_router(addons.router)
 router.include_router(panel.router)
-
 __all__ = ["router"]
