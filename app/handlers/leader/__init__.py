7fd729e0bef5516a99f55f1034ed85b91b665b8f
davidbagh22 <davidbagh22@gmail.com>
2026-07-02 02:37:47 +0400

    Enable block6 leader event constructor

--- app/handlers/leader/__init__.py
+++ app/handlers/leader/__init__.py
@@ -1,6 +1,7 @@
 from aiogram import Router
-from app.handlers.leader import open_tasks, panel, task_deadline_buttons
+from app.handlers.leader import open_tasks, events_block6, panel, task_deadline_buttons
 router = Router(name="leader_root")
 router.include_router(open_tasks.router)
 router.include_router(task_deadline_buttons.router)
+router.include_router(events_block6.router)
 router.include_router(panel.router)
