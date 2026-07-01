33265fc6b327aa0a0e59284b538790b816d0cf71
davidbagh22 <davidbagh22@gmail.com>
2026-07-02 01:16:03 +0400

    Restore leader routers

--- app/handlers/leader/__init__.py
+++ app/handlers/leader/__init__.py
@@ -1,9 +1,6 @@
 from aiogram import Router
-from app.handlers.leader import event_builder, addons, open_tasks, panel, task_deadline_buttons
+from app.handlers.leader import open_tasks, panel, task_deadline_buttons
 router = Router(name="leader_root")
-router.include_router(event_builder.router)
 router.include_router(open_tasks.router)
 router.include_router(task_deadline_buttons.router)
-router.include_router(addons.router)
 router.include_router(panel.router)
-__all__ = ["router"]
