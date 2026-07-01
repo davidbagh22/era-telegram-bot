e8413e1a9ac61ef3986d2ca956d24b25670ea0ef
davidbagh22 <davidbagh22@gmail.com>
2026-07-01 22:27:17 +0400

    Include admin addon handlers

--- app/handlers/admin/__init__.py
+++ app/handlers/admin/__init__.py
@@ -1,3 +1,9 @@
-from app.handlers.admin.panel import router
+from aiogram import Router
+
+from app.handlers.admin import addons, panel
+
+router = Router(name="admin_root")
+router.include_router(addons.router)
+router.include_router(panel.router)
 
 __all__ = ["router"]
