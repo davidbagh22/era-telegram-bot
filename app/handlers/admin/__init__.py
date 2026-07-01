cf7050ac0b81db7f26daaacfcb8eb6f78b37ff11
davidbagh22 <davidbagh22@gmail.com>
2026-07-02 02:37:30 +0400

    Enable block6 admin event workflow

--- app/handlers/admin/__init__.py
+++ app/handlers/admin/__init__.py
@@ -1,5 +1,5 @@
 from aiogram import Router
-from app.handlers.admin import task_review_block2, user_profile_block3_safe, projects_block5_list, projects_block5_decision, projects_block5_team, panel
+from app.handlers.admin import task_review_block2, user_profile_block3_safe, projects_block5_list, projects_block5_decision, projects_block5_team, events_block6, panel
 
 router = Router(name="admin_root")
 router.include_router(task_review_block2.router)
@@ -7,6 +7,7 @@ router.include_router(user_profile_block3_safe.router)
 router.include_router(projects_block5_list.router)
 router.include_router(projects_block5_decision.router)
 router.include_router(projects_block5_team.router)
+router.include_router(events_block6.router)
 router.include_router(panel.router)
 
 __all__ = ["router"]
