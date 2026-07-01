d5c344659e3a07c151046b4a01f2ef4db7b25023
davidbagh22 <davidbagh22@gmail.com>
2026-07-02 01:15:12 +0400

    Restore admin router to last stable version

--- app/handlers/admin/__init__.py
+++ app/handlers/admin/__init__.py
@@ -1,36 +1,3 @@
-from aiogram import Router
-
-from app.handlers.admin import (
-    dashboard_start,
-    user_reward_direct,
-    reward_exchange,
-    event_flow,
-    dashboard_quick,
-    task_guard2,
-    event_points_check,
-    activity_files,
-    activity_award_check,
-    task_review_clean,
-    task_review,
-    project_team_review,
-    addons,
-    panel,
-)
-
-router = Router(name="admin_root")
-router.include_router(dashboard_start.router)
-router.include_router(user_reward_direct.router)
-router.include_router(reward_exchange.router)
-router.include_router(event_flow.router)
-router.include_router(dashboard_quick.router)
-router.include_router(task_guard2.router)
-router.include_router(event_points_check.router)
-router.include_router(activity_files.router)
-router.include_router(activity_award_check.router)
-router.include_router(task_review_clean.router)
-router.include_router(task_review.router)
-router.include_router(project_team_review.router)
-router.include_router(addons.router)
-router.include_router(panel.router)
+from app.handlers.admin.panel import router
 
 __all__ = ["router"]
