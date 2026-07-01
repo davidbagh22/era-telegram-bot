274971ecd0872ddab4235001dbda0a7fafb5bad6
davidbagh22 <davidbagh22@gmail.com>
2026-07-01 22:25:58 +0400

    Include leader addon handlers

--- app/handlers/leader/__init__.py
+++ app/handlers/leader/__init__.py
@@ -1,10 +1,11 @@
 from aiogram import Router
 
-from app.handlers.leader import open_tasks, panel, task_deadline_buttons
+from app.handlers.leader import addons, open_tasks, panel, task_deadline_buttons
 
 router = Router(name="leader_root")
 router.include_router(open_tasks.router)
 router.include_router(task_deadline_buttons.router)
+router.include_router(addons.router)
 router.include_router(panel.router)
 
 __all__ = ["router"]
