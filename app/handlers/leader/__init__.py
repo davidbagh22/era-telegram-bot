3238c0fba8a8d1f5f045660622ba138397d07a2e
davidbagh22 <davidbagh22@gmail.com>
2026-07-02 00:08:08 +0400

    Register leader event builder

--- app/handlers/leader/__init__.py
+++ app/handlers/leader/__init__.py
@@ -1,8 +1,9 @@
 from aiogram import Router
 
-from app.handlers.leader import addons, open_tasks, panel, task_deadline_buttons
+from app.handlers.leader import event_builder, addons, open_tasks, panel, task_deadline_buttons
 
 router = Router(name="leader_root")
+router.include_router(event_builder.router)
 router.include_router(open_tasks.router)
 router.include_router(task_deadline_buttons.router)
 router.include_router(addons.router)
