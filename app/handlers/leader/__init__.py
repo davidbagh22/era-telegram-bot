ac35780f6df640379a5e181644ba36d358996b94
davidbagh22 <davidbagh22@gmail.com>
2026-07-01 21:04:13 +0400

    Include open task router

--- app/handlers/leader/__init__.py
+++ app/handlers/leader/__init__.py
@@ -1,8 +1,9 @@
 from aiogram import Router
 
-from app.handlers.leader import panel, task_deadline_buttons
+from app.handlers.leader import open_tasks, panel, task_deadline_buttons
 
 router = Router(name="leader_root")
+router.include_router(open_tasks.router)
 router.include_router(task_deadline_buttons.router)
 router.include_router(panel.router)
 
