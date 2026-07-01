36344c0aa22cde7746e54f749f69f2a741185208
davidbagh22 <davidbagh22@gmail.com>
2026-07-01 20:15:24 +0400

    Include task deadline button router

--- app/handlers/leader/__init__.py
+++ app/handlers/leader/__init__.py
@@ -1,3 +1,9 @@
-from app.handlers.leader.panel import router
+from aiogram import Router
+
+from app.handlers.leader import panel, task_deadline_buttons
+
+router = Router(name="leader_root")
+router.include_router(task_deadline_buttons.router)
+router.include_router(panel.router)
 
 __all__ = ["router"]
