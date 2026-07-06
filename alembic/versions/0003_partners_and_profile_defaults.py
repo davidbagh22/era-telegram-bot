5ad400ebb2ee9e1c4e95258d189847c7fc854734
davidbagh22 <davidbagh22@gmail.com>
2026-07-07 03:11:29 +0400

    Add migration placeholder

---
+++ alembic/versions/0003_partners_and_profile_defaults.py
@@ -0,0 +1,17 @@
+"""Add partners and repair profile timestamp defaults."""
+
+from alembic import op
+import sqlalchemy as sa
+
+revision = "0003_partners_and_profile_defaults"
+down_revision = "0002_social_profiles"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
