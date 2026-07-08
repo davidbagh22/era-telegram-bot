40e3acf894ec12007cbf50a2a19aad9242155c1a
davidbagh22 <davidbagh22@gmail.com>
2026-07-08 22:15:22 +0400

    Merge alembic heads after partners seed

---
+++ alembic/versions/0005_merge_heads.py
@@ -0,0 +1,14 @@
+"""Merge era v2 foundation and partners branches."""
+
+revision = "0005_merge_heads"
+down_revision = ("0002_era_v2_foundation", "0004_seed_default_partners")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
