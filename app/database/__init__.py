4c1a4bad6adbe13614e098d7521db14afe92492e
davidbagh22 <davidbagh22@gmail.com>
2026-07-08 22:15:16 +0400

    Load chat moderation model

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -2,5 +2,6 @@ from app.database.base import Base
 from app.database.models import *  # noqa: F403
 import app.database.socials  # noqa: F401
 import app.database.partners  # noqa: F401
+import app.database.chat_moderation  # noqa: F401
 
 __all__ = ["Base"]
