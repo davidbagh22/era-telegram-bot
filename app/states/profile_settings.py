c7fb687e099eba63f3afcbe0cbcf0e300d19c23a
davidbagh22 <davidbagh22@gmail.com>
2026-07-10 15:44:31 +0400

    Expand profile settings FSM states

--- app/states/profile_settings.py
+++ app/states/profile_settings.py
@@ -2,6 +2,10 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class ProfileSettingsStates(StatesGroup):
+    text_value = State()
+    birth_date = State()
+    phone = State()
+    email = State()
     photo = State()
     social_url = State()
     contact_email = State()
