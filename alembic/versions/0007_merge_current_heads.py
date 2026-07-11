a8f8cf07c2a19ceebb5ddc825721c593319304cf
davidbagh22 <davidbagh22@gmail.com>
2026-07-12 02:25:51 +0400

    Add merge migration placeholder

---
+++ alembic/versions/0007_merge_current_heads.py
@@ -0,0 +1,14 @@
+"""Merge current Alembic heads."""
+
+revision = "0007_merge_current_heads"
+down_revision = ("0006_chat_moderation", "0004_user_birth_date")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
