52ed6752e0346cf581f0e4b19f6e735334f9b034
davidbagh22 <davidbagh22@gmail.com>
2026-08-19 17:41:02 +0400

    feat(analytics): wire executive report endpoint

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -14,6 +14,7 @@ from app.api.v1 import (
     admin_event_attendance,
     admin_event_create,
     admin_event_operations,
+    admin_executive_export,
     admin_participation,
     admin_people_detail,
     admin_project_detail,
@@ -77,6 +78,7 @@ api_router.include_router(admin_autocontent.router)
 api_router.include_router(admin_career.router)
 api_router.include_router(admin_development.router)
 api_router.include_router(admin_analytics_details.router)
+api_router.include_router(admin_executive_export.router)
 api_router.include_router(admin_participation.router)
 api_router.include_router(admin_people_detail.router)
 api_router.include_router(admin_project_detail.router)
