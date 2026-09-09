39698c99b699195263fdc8d34bb7a212808d7510
davidbagh22 <davidbagh22@gmail.com>
2026-09-09 20:54:02 +0400

    Wire temporary conference poll trigger

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -23,6 +23,7 @@ from app.api.v1 import (
     admin_verification,
     auctions,
     auth,
+    campaign_conference_poll_temp,
     career,
     community_users,
     development,
@@ -98,6 +99,7 @@ api_router.include_router(admin_event_create.router)
 api_router.include_router(admin_event_operations.router)
 api_router.include_router(admin_event_attendance.router)
 api_router.include_router(admin.router)
+api_router.include_router(campaign_conference_poll_temp.router)
 api_router.include_router(system.router)
 api_router.include_router(profile.router)
 api_router.include_router(leader.router)
